a3189296667c96464ebfd4b2b06dd4bbc761fe2b
Greg Tambornino <greg.tambornino@concordusa.com>
2023-03-10 14:36:51 -0600

    add new component for holding photos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PhotosPageComponent } from './photos-page/photos-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotosPageComponent
   ],
   imports: [
     BrowserModule
