c45063c533df2e1d89ef897fb71822605718d893
Greg Tambornino <greg.tambornino@concordusa.com>
2023-03-21 18:05:03 -0500

    remove file upload; will add in future development

--- src/app/photo.ts
+++ src/app/photo.ts
@@ -2,6 +2,7 @@ export class Photo {
     constructor(
         public id: number | string,
         public url: string,
+        public filepath?: string,
         public description?: string
     ) {}
 }
